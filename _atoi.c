92750eab03700627b14b98983752d7f2e2986452
eskainah <eskainah@gmail.com>
2022-11-15 22:49:49 +0200

    updated

--- _atoi.c
+++ _atoi.c
@@ -73,7 +73,8 @@ int _atoi(char *s)
 			{
 				flag = 1;				
 				result *= 10;					
-				result += (s[i] - '0');																		}
+				result += (s[i] - '0');
+			}
 			else if (flag == 1)		
 				flag = 2;
 		}
